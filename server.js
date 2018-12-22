5e19f6ef9479bf75f949c48f494c27be078a0a77
buitahau@gmail.com <buitahau@gmail.com>
2018-12-22 21:26:14 +0700

    revise

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
 const routes = require('./routers/routes');
-var cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/frontend'));
 
