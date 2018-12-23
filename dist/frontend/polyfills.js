c4aff07e89271d7c8af18e99841d581263a1d929
Khanh Tran <bluesky020990>
2018-12-23 20:54:01 +0700

    Update Front-end.

--- dist/frontend/polyfills.js
+++ dist/frontend/polyfills.js
@@ -5497,8 +5497,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! X:\ws_learning\clientvietpower\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! X:\ws_learning\clientvietpower\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! G:\Workspace\clientvietpower\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! G:\Workspace\clientvietpower\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
