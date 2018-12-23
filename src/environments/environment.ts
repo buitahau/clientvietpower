17be6dbe093506e13e175817d016bff2888d2201
buitahau@gmail.com <buitahau@gmail.com>
2018-12-23 10:01:14 +0700

    revise

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   settings: {
-    serverendpoint: 'http://localhost:8080/server/api/',
-    // serverendpoint: 'http://ec2-34-227-171-6.compute-1.amazonaws.com:8081/demoson/server/api',
+    serverendpoint: 'http://ec2-34-227-171-6.compute-1.amazonaws.com:8081/demoson/server/api',
     dateFormat : 'dd/MM/yyyy'
   }
 };
