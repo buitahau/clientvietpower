eee37767ce8d50769c789ffa2899327465a75827
buitahau@gmail.com <buitahau@gmail.com>
2018-12-27 06:15:05 +0700

    revise environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,21 @@
 export const environment = {
-  production: true,
+  production: false,
   settings: {
-    serverendpoint: 'http://localhost:8080/server/api/'
+    // serverendpoint: 'http://localhost:8080/server/api/',
+    serverendpoint: 'http://ec2-34-227-171-6.compute-1.amazonaws.com:8081/demoson/server/api/',
+    dateFormat : 'dd/MM/yyyy'
   }
 };
+
+export class StaticVariable {
+  private static _settings;
+  constructor() {
+    StaticVariable._settings = environment.settings;
+  }
+
+  public STATIC_VARIABLE = StaticVariable;
+
+  static get settings() {
+    return this._settings;
+  }
+}
