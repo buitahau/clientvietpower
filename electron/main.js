8f1feaae53ac9913d6bfba736452be0e99a907b7
buitahau@gmail.com <buitahau@gmail.com>
2019-02-14 22:16:51 +0700

    revise

--- electron/main.js
+++ electron/main.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
+const urlConfig = './config.json';
 
 function createWindow () {
   // Create the browser window.
